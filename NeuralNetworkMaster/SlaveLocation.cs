e0714ca907595cc96c2bdc3a198b19b89b5d71cb
Kishan N <kishann380@gmail.com>
2018-02-26 21:52:29 +0530

    Added SlaveLocation class.
    -CommunicationClass Has many SlaveLocation class used to receivein the form of JSON.

---
+++ NeuralNetworkMaster/SlaveLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworkMaster
+{
+    class SlaveLocation
+    {
+        public string Id { get; set; }
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+    }
+}
