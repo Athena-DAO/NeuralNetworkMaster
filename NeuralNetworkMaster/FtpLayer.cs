1b9bf30d727e000771f7ac3f04212da9227a542f
Kishan N <kishann380@gmail.com>
2018-02-26 14:13:11 +0530

    Added FTP Layer.
    -FTP Layer downloads the file from ftp server.

---
+++ NeuralNetworkMaster/FtpLayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using FluentFTP;
+using System.Text;
+using System.IO;
+
+namespace NeuralNetworkMaster
+{
+    class FtpLayer
+    {
+
+
+        private FtpClient client;
+        public FtpLayer()
+        {
+            client = new FtpClient("127.0.0.1");
+
+        }
+
+
+        public void DownloadFile(String fileName)
+        {
+
+            client.Connect();
+            client.DownloadFile(Directory.GetCurrentDirectory() + "//FileStore//" + fileName, "//" + fileName);
+            client.Disconnect();
+        }
+    }
+}
