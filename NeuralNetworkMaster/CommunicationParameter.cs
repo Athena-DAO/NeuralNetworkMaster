72ada1e51746948c9971d4c92757aecd60e91b81
Kishan N <kishann380@gmail.com>
2018-02-26 21:48:35 +0530

    CommunicationParameter class added
    -The class contains properties to receive in the form of JSON.

---
+++ NeuralNetworkMaster/CommunicationParameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworkMaster
+{
+    class CommunicationParameter
+    {
+        public string Id { get; set; }
+        public int NumberOfSlaves { get; set; }
+        public int Port { get; set; }
+        public SlaveLocation[] SlaveLocations { get; set; }
+    }
+}
